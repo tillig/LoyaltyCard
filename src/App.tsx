3038e83118a555f09d95ab1102917bae75bbb66c
Travis Illig <tillig@paraesthesia.com>
2021-08-26 11:09:30 -0700

    Simple bar code entry form.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import { BarCodeForm } from './BarCodeForm';
 import './App.css';
 
 // TODO: Switch barcode generation to https://www.npmjs.com/package/jsbarcode
-// TODO: Dynamic form to add up to eight barcodes.
+// TODO: Set up BarCodeForm to update history as it's changed.
+// TODO: BarCodeForm doesn't want to be full width. Why not?
 // TODO: Get PDF rendering using http://mozilla.github.io/pdf.js/
 // TODO: History support if it's not already baked in (so back/forward and bookmark works).
 
@@ -18,10 +22,16 @@ import './App.css';
 export class App extends React.Component {
   /**
    * Render the form.
-   * @param {Properties} props The properties for the wizard.
+   * @param {Properties} props The properties for the application.
    * @return {React.ReactNode} The node to render.
    */
   render(): JSX.Element | null {
+    const barCodeForms = [];
+
+    for (let index = 0; index < 8; index++) {
+      barCodeForms.push(<BarCodeForm key={index} />);
+    }
+
     return (
       <div>
         <header>
@@ -45,13 +55,21 @@ export class App extends React.Component {
                   <h1>LoyaltyCard <small className="text-muted">Generate a combined loyalty/club card</small></h1>
                 </div>
                 <p><strong>Ready to save some wallet space?</strong></p>
-                <p><strong>Use the form below to enter up to eight (8) barcodes for your loyalty cards.</strong> As you save the data, you can preview the output to double-check that the barcode type and format is as expected. It&apos;s important to compare this to the real barcode or it won&apos;t work when it&apos;s scanned later.</p>
+                <p><strong>Use the form below to enter up to eight (8) bar codes for your loyalty cards.</strong> As you save the data, you can preview the output to double-check that the barcode type and format is as expected. It&apos;s important to compare this to the real barcode or it won&apos;t work when it&apos;s scanned later.</p>
                 <p><strong>When finished...</strong></p>
                 <ul>
-                  <li><strong>Use the &quot;download&quot; button to get the rendered combined card as a PDF for printing.</strong> You&apos;ll have a single card with up to four barcodes on each side, suitable for lamination.</li>
+                  <li><strong>Use the &quot;download&quot; button to get the rendered combined card as a PDF for printing.</strong> You&apos;ll have a single card with up to four bar codes on each side, suitable for lamination.</li>
                   <li><strong>&quot;Save&quot; your data by bookmarking the page.</strong> When you return, you can re-print your card or edit/update.</li>
                 </ul>
               </Col>
+            </Row>
+            <Row>
+              <Form>
+                {barCodeForms}
+                <Button variant="primary">
+                  Download PDF
+                </Button>
+              </Form>
             </Row>
           </Container>
         </main>
@@ -61,7 +79,7 @@ export class App extends React.Component {
               <div className="fb-like" data-href="https://www.paraesthesia.com/LoyaltyCard/" data-send="true" data-width="450" data-show-faces="true"></div>
             </div>
             <p className="disclaimer">LoyaltyCard generator created by <a href="https://www.paraesthesia.com">Travis Illig</a>. Phone-based loyalty card applications have gotten better, but sometimes you just need that piece of paper.</p>
-            <p className="disclaimer"><a href="https://www.tec-it.com/" target="_blank" rel="noreferrer">Barcodes generated with TEC-IT Barcode Software.</a></p>
+            <p className="disclaimer"><a href="https://www.tec-it.com/" target="_blank" rel="noreferrer">Bar codes generated with TEC-IT Barcode Software.</a></p>
 
             <ins className="adsbygoogle"
               style={{ display: 'inline-block', width: '468px', height: '60px' }}
